91e3ffc1003b3428f3a701937add294137c17d91
Ben Biddington <ben.biddington@gettimely.com>
2025-08-09 11:55:19 +1200

    [DRAFT] How to allow 'Page.startScreencast' to work

---
+++ lib/PuppeteerSharp.Tests/CDPSessionTests/ScreencastingTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PuppeteerSharp.Nunit;
+
+namespace PuppeteerSharp.Tests.CDPSessionTests;
+
+sealed class ScreencastingTests : PuppeteerPageBaseTest
+{
+    [Test, PuppeteerTest("StartScreencast.spec", "Page.startScreencast", "should send events")]
+    public async Task ShouldSendEvents()
+    {
+        var client = await Page.CreateCDPSessionAsync();
+        var events = new List<MessageEventArgs>();
+
+        client.MessageReceived += (_, e) =>
+        {
+            if (e.MessageID == "Page.screencastFrame")
+            {
+                events.Add(e);
+            }
+        };
+
+        await client.SendAsync("Network.enable");
+        await client.SendAsync("Page.startScreencast");
+
+        await Page.GoToAsync(TestConstants.EmptyPage);
+
+        await client.SendAsync("Page.stopScreencast");
+        await client.DetachAsync();
+
+        foreach (var e in events)
+        {
+            Console.WriteLine(e.MessageID);
+            Console.WriteLine(e.MessageData);
+        }
+
+        Assert.That(events, Is.Not.Empty);
+    }
+}
